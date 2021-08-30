9c3e375a5171e59809ddc4402fbec1c8c8dfa4db
Anguis Nox <carlos.0111rivas@gmail.com>
2021-08-30 15:43:32 -0600

    Signed-off-by: Anguis Nox <carlos.0111rivas@gmail.com>

---
+++ Periodo 1/Ejercicios Semana 3/calculadora.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	float x;
+	float y;
+	float r;
+	char op;
+	
+	cout << "Calculadora sensilla" << endl;
+	cout << "Numero 1: ";
+	cin >> x;
+	cout << "Numero 2: ";
+	cin >> y;
+	cout << "Operacion (+|-|*|/): ";
+	cin >> op;
+	
+	switch(op){
+		case '+':
+			r = x + y;
+			break;
+		case '-':
+			r = x - y;
+			break;
+		case '*':
+			r = x * y;
+			break;
+		case '/':
+			r = x / y;
+			break;
+		default:
+			cout << "Operador no valido" << endl;
+		
+	}
+	
+	cout << "x " << op << " y = " << r << endl;
+	
+	return 0;
+}
